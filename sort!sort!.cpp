66a3c13562a3cdbe501df75f276aa7b1100423e0
Gibson Chen <gibson.chen-giqh@asml.com>
2026-07-27 13:17:46 +0800

    use copilot to solve the sort! sort! use algorithm(sort) and vector <int> and use sort(begin, end, cmp). bool cmp

--- sort!sort!.cpp
+++ sort!sort!.cpp
@@ -1,13 +1,63 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
-int main(){
-    int M, N;
-    cin >> N >> M;
-    int next[10001];
+int M;   // 設成全域變數，讓 cmp 可以使用
+
+bool Odd(int x) {
+    return x % 2 != 0;
+}
+
+bool cmp(int a, int b) {
+    int ra = a % M;
+    int rb = b % M;
+
+    // 1. 餘數小的排前面
+    if (ra != rb) {
+        return ra < rb;
+    }
+
+    // 2. 餘數一樣時，判斷原本的數字是奇數還是偶數
+    bool aOdd = Odd(a);
+    bool bOdd = Odd(b);
+
+    // 一奇一偶，奇數排前面
+    if (aOdd != bOdd) {
+        return aOdd;
+    }
+
+    // 3. 都是奇數，數值大的排前面
+    if (aOdd && bOdd) {
+        return a > b;
+    }
+
+    // 4. 都是偶數，數值小的排前面
+    return a < b;
+}
+
+int main() {
+    int N;
+
+    while (cin >> N >> M) {
+        cout << N << " " << M << "\n";
+
+        if (N == 0 && M == 0) {
+            break;
+        }
+
+        vector<int> num(N);
+
+        for (int i = 0; i < N; i++) {
+            cin >> num[i];
+        }
+
+        sort(num.begin(), num.end(), cmp);
+
+        for (int x : num) {
+            cout << x << "\n";
+        }
+    }
+
     return 0;
 }
-    
