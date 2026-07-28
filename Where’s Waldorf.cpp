7a7af2556a99a908ed1dc638c913f83fabf93875
Gibson Chen <gibson.chen-giqh@asml.com>
2026-07-28 15:00:14 +0800

    offboard document need to sign

---
+++ Where’s Waldorf.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int situ;
+    cin >> situ;
+    int n, m;
+    
+    while (cin >> n >> m){
+        string line;
+        vector<char> str;
+        for (int i = 0; i < n; i++){
+            getline(cin, line); 
+        }
+        
+    }
+    return 0;
+}
